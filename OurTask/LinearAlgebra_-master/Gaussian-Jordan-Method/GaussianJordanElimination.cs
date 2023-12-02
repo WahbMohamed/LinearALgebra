65144a09cee0d0d8533b12db42ce9be2f6f7f147
WahbMohamed <121579875+WahbMohamed@users.noreply.github.com>
2023-12-02 18:30:58 +0200

    Add files via upload

---
+++ OurTask/LinearAlgebra_-master/Gaussian-Jordan-Method/GaussianJordanElimination.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class GaussianJordanElimination
+    {
+        private double[,] a;
+        private int rows;
+        private int cols;
+
+        public GaussianJordanElimination()
+        {
+        }
+
+        public void SetArray(double[,] arr)
+        {
+            this.a = arr;
+            this.rows = arr.GetLength(0);
+            this.cols = arr.GetLength(1);
+        }
+
+        public void SwapRows(ref double[,] arr, int row)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (row + 1 < rows)
+                {
+                    double temp = arr[row, col];
+                    arr[row, col] = arr[row + 1, col];
+                    arr[row + 1, col] = temp;
+                }
+            }
+        }
+
+        public string MatToString(double[,] mat)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    sb.Append(mat[i, j] + "  ");
+                }
+                sb.Append("\n");
+            }
+            sb.Append("\n");
+            return sb.ToString();
+        }
+
+        public string Solve()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Displaying input Matrix \n");
+            sb.Append(MatToString(a));
+
+            bool CheckNoSolution()
+            {
+                int count;
+                for (int i = 0; i < rows; i++)
+                {
+                    count = 0;
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (a[i, j] == 0)
+                        {
+                            count++;
+                            if (count == cols && j + 1 < cols)
+                            {
+                                if (a[i, j + 1] != 0)
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+
+            bool CheckInfiniteSolutions()
+            {
+                int count;
+                for (int i = 0; i < rows; i++)
+                {
+                    count = 0;
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (a[i, j] == 0)
+                        {
+                            count++;
+                            if (count == cols + 1)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+
+            if (CheckInfiniteSolutions())
+            {
+                sb.Append("The system has an infinite set of solutions.\n");
+                return sb.ToString();
+            }
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (a[i, i] == 0)
+                {
+                    SwapRows(ref a, i);
+
+                    sb.Append($"Interchange R{i + 1} <-> R{i + 2}\n");
+
+                    break;
+                }
+
+                if (CheckNoSolution())
+                {
+                    sb.Append("The system has no solution.\n");
+                    return sb.ToString();
+                }
+
+
+                double temp = a[i, i];
+
+                for (int j = 0; j < cols; j++)
+                {
+                    if (temp != 0)
+                        a[i, j] = a[i, j] / temp;
+                }
+
+                sb.Append($"1/{temp} * R{i + 1} --> R{i + 1}");
+                sb.Append(MatToString(a));
+
+                for (int k = 0; k < rows; k++)
+                {
+                    if (i != k)
+                    {
+                        temp = a[k, i];
+                        for (int j = 0; j < cols; j++)
+                        {
+                            a[k, j] = a[k, j] - (temp * a[i, j]);
+                        }
+
+                        if (temp != 0)
+                        {
+                            if (temp == 1)
+                            {
+                                sb.Append($"R{k + 1} - R{i + 1} --> R{k + 1}\n");
+                                sb.Append(MatToString(a));
+                            }
+                            else
+                            {
+                                sb.Append($"R{k + 1} * {temp} - R{i + 1} --> R{k + 1}\n");
+                                sb.Append(MatToString(a));
+
+                            }
+                        }
+                    }
+                }
+                sb.Append("Solved matrix \n");
+                sb.Append(MatToString(a));
+
+                int numVariables = cols - 1;
+
+                for (int m = 0; m < rows; m++)
+                {
+                    sb.Append($"x{m + 1} = {a[m,cols-1]}\n");
+                }
+            }
+            return sb.ToString();
+        }
+       
+     
+    }
+}
---
+++ OurTask/LinearAlgebra_-master/Gaussian-Jordan-Method/MainGUI.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace ConsoleApplication6
+{
+    // YAHIA 
+    public partial class MainGUI : Form
+    {
+        GaussianJordanElimination gaus = new GaussianJordanElimination();
+        private int det;
+
+        public MainGUI()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void MainGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private DataTable createTable(int rows, int cols)
+        {
+            DataTable table = new DataTable();
+            for (int x = 0; x < cols; x++)
+            {
+                table.Columns.Add("X" + x, typeof(double));
+            }
+            for (int y = 0; y < rows; y++)
+            {
+                table.Rows.Add(); // Add empty rows
+            }
+            return table;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rows = 0;
+            int cols = 0;
+
+            try
+            {
+                cols = int.Parse(textBox1.Text)+1;
+                rows = int.Parse(textBox2.Text);
+
+                if (cols <= 0 || rows <= 0)
+                {
+                    MessageBox.Show("Rows and Columns must be a positive integer");
+                }
+
+                DataTable table = createTable(rows, cols);
+                dataGridView1.Visible = true;
+                dataGridView1.DataSource = table;
+                dataGridView1.Size = new Size((cols * 70) + 30, (rows * 25) + 15); // Adjusted column width
+                Console.WriteLine(rows + " " + cols);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Rows and Columns must be an integer");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable table = (DataTable)dataGridView1.DataSource;
+            int rows = table.Rows.Count;
+            int cols = table.Columns.Count;
+            double[,] data = new double[rows, cols];
+
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    if (!double.TryParse(table.Rows[y][x].ToString(), out data[y, x]))
+                    {
+                        MessageBox.Show("Invalid input at row " + (y + 1) + ", column " + (x + 1));
+                        return;
+                    }
+                }
+            }
+
+            //double det = GaussianJordanElimination.DET(rows, data);
+
+
+
+
+
+            gaus.SetArray(data);
+            string solution = gaus.Solve();
+
+                richTextBox1.Text = "Step 1: Create the Augmented Matrix\n";
+                richTextBox1.Text += "The augmented matrix for this system is:\n";
+                richTextBox1.Text += gaus.MatToString(data) + "\n";
+
+                if (solution.Contains("infinite set of solutions"))
+                {
+                    richTextBox1.Text += "Step 2: Perform Gaussian Jordan Elimination\n";
+                    richTextBox1.Text += "\n";
+                    richTextBox1.Text += " the matrix transformations step by step.\n\n";
+                    richTextBox1.Text += "Step-by-Step Gaussian Jordan Elimination:\n";
+                    richTextBox1.Text += solution;
+                }
+                else
+                {
+                    richTextBox1.Text += "Solution:\n";
+                    richTextBox1.Text += solution;
+                    //richTextBox1.Text += "\nNow, let's determine the nature of the solution based on the structure of the augmented matrix to see if it has no solution or an infinite set of solutions.";
+                    richTextBox1.Text += "DONE";
+                }
+            
+        }
+
+
+                private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
